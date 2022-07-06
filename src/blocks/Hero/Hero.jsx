910bf80749bc480ac5d4c683c75b23e00f410f60
Kykeon <paulinoandresmiguel@gmail.com>
2022-07-05 23:56:07 -0400

    Create mobile hero image

--- src/blocks/Hero/Hero.jsx
+++ src/blocks/Hero/Hero.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import heroMobile from 'src/assets/images/homepage/hero-bg-mobile.jpg';
+import { windowSize } from '../../utils';
+import heroMobile from '../../assets/images/homepage/hero-bg-mobile.jpg';
+import heroTablet from '../../assets/images/homepage/hero-bg-tablet.jpg';
+import heroDesktop from '../../assets/images/homepage/hero-bg-desktop.jpg';
 
 function Hero() {
+  const { width } = windowSize();
+  const heroImage = width < 768 ? heroMobile : width < 1024 ? heroTablet : heroDesktop;
+
   return (
-    <div>
-      <div className='hero-bg-mobile' style={{ backgroundImage: `url(${heroMobile})` }}>
-        <div className='hero-bg-mobile__content'>
-          <h1 className='hero-bg-mobile__title'>Dine</h1>
+    <>
+      <section className='bg-primary-gray'>
+        <div className=''>
+          <div>
+            <img src={heroImage} alt='pizza hero' className='mx-auto' />
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      </section>
+    </>
+  ); 
 }
 
 export default Hero;
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export { default as windowSize } from './windowSize';
