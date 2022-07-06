28e9df47c2be895f4b6aa25d819a2625e4e93273
Kykeon <paulinoandresmiguel@gmail.com>
2022-07-05 23:35:45 -0400

    Configure react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Booking from './pages/Booking';
 
 function App() {
   return (
-    <Router>
-      <Route>
-        <Route path='/' exact component={Home} />
-        <Route path='/booking' component={Booking} />
-      </Route>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking" element={<Booking />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
