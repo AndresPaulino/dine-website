33dcf70964aeb7f83109e91f62a9c278849b3385
Kykeon <paulinoandresmiguel@gmail.com>
2022-07-07 00:24:08 -0400

    Add button to dynamic tab component

--- src/blocks/Buttons/LightButton/LightButton.jsx
+++ src/blocks/Buttons/LightButton/LightButton.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 function LightButton() {
   return (
-    <button type='button' className='btn btn-Light'>
-      Normal Light
+    <button type='button' className='btn bg-black text-white uppercase border-[1px] font-semi px-16 py-5 tracking-widest'>
+      Book a table
     </button>
   );
 }
--- src/pages/Home/components/TableEvents/TableEvents.jsx
+++ src/pages/Home/components/TableEvents/TableEvents.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { FamilyGathering, SocialEvents, SpecialEvents } from './components';
 import { windowSize } from 'utils';
-import { DarkButton } from 'blocks/Buttons';
+import { LightButton } from 'blocks/Buttons';
 
 function TableEvents() {
   const [activeTab, setActiveTab] = useState('Family Gathering');
@@ -15,6 +15,11 @@ function TableEvents() {
       {activeTab === 'Family Gathering' && <FamilyGathering active={activeTab} handleActive={handleActive} />}
       {activeTab === 'Special Events' && <SpecialEvents active={activeTab} handleActive={handleActive} />}
       {activeTab === 'Social Events' && <SocialEvents active={activeTab} handleActive={handleActive} />}
+      <div>
+        <div className='flex justify-center py-12'>
+          <LightButton className='w-full' onClick={() => {}} />
+        </div>
+      </div>
     </section>
   );
 }
